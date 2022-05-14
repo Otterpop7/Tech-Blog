40cb0644eff71bd82d44f6c4b96ad3bfd5305f17
Otterpop7 <Jordanotterson7@gmail.com>
2022-05-14 10:13:59 -0600

    heroku deployment

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const session = require("express-session")
 const routes = require("./controllers")
 const myHelpers = require("./utils/helpers")
 
+const app = express()
+const PORT = process.env.PORT || 3001
+
 const sequelize = require("./config/connection")
 //Loads the handlebars module
 const handlebars = require("express-handlebars")
 const SequelizeStore = require("connect-session-sequelize")(session.Store)
-
-const app = express()
-const PORT = process.env.PORT || 3001
 
 const sess = {
   secret: "Super secret secret",
